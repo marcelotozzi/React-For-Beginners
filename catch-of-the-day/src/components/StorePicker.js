5ffd83c93e89c93140c9497baec3404cfd2eadbd
Marcelo Tozzi <marcelo.lima@buscapecompany.com>
2017-06-10 01:37:34 -0300

    9-12 watched

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,17 +1,33 @@
 import React from 'react'
+import {getFunName} from '../helpers'
 
 class StorePicker extends React.Component {
+	// constructor(){
+	// 	super()
+	// 	this.goToStore = this.goToStore.bind(this)
+	// }
+	goToStore(event){
+		event.preventDefault()
+		const storeId = this.storeInput.value
+		console.log(`Going to ${storeId}`)
+		this.context.router.transitionTo(`/store/${storeId}`)
+	}
 	render(){
 		// Eita fi
 		return (
-		<form className="store-selector">
-			{/* Comente aqui */}
+		<form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
+			{/* Usar funcao no onSubmit no lugar do construtor */ }
 			<h2>Please Enter A Store</h2>
-			<input type="text" required placeholder="Store Name"/>
+			<input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => {this.storeInput = input}}/>
 			<button type="submit"> Visit Store -></button>
 		</form>
 	  ) 
 	}
+}
+
+// usado para acessar o router, que esta no contexto por causa do BrowserRouter no index.js
+StorePicker.contextTypes = {
+	router: React.PropTypes.object
 }
 
 export default StorePicker
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,10 +1,26 @@
 import React from 'react'
 // importa só o metodo e não todo o react-dom
 import { render } from 'react-dom'
+import  { BrowserRouter, Match, Miss} from 'react-router'
+
 // nao seria melhor importar dentro do component?
 import './css/style.css'
 
 import App from './components/App'
+import NotFound from './components/NotFound'
 import StorePicker from './components/StorePicker'
 
-render(<App/>, document.querySelector('#main'))
+const Root = () => {
+	return (
+		<BrowserRouter>
+			<div>
+				<Match exactly pattern="/" component={StorePicker} />
+				<Match exactly pattern="/store/:storeId" component={App} />
+				<Miss component={NotFound} />
+			</div>
+		</BrowserRouter>
+	)
+}
+
+render(<Root/>, document.querySelector('#main'))
+
