7974014ae57b176c9ac69604f7dac40ca2ebdec8
Marcelo Tozzi <marcelo.lima@buscapecompany.com>
2017-06-09 23:52:54 -0300

    1-8 watched

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,10 @@
-// let's go!
+import React from 'react'
+// importa só o metodo e não todo o react-dom
+import { render } from 'react-dom'
+// nao seria melhor importar dentro do component?
+import './css/style.css'
+
+import App from './components/App'
+import StorePicker from './components/StorePicker'
+
+render(<App/>, document.querySelector('#main'))
