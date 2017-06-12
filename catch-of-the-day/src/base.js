b0a4ce26175cb6f82c36c08aad1b21b1b607a4ec
Marcelo Tozzi <marcelo.lima@buscapecompany.com>
2017-06-12 18:42:32 -0300

    13-20 watched

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+	apiKey: "AIzaSyArmQY4hZn1qwp-1UJfq7EVKMA_NqIUrOI",
+  authDomain: "catch-the-day-tozzi.firebaseapp.com",
+  databaseURL: "https://catch-the-day-tozzi.firebaseio.com"
+});
+
+export default base;
